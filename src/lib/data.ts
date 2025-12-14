636fdfe7653c9a6236ac088e036d4f36ed789812
Jacob Maynard <jacobamaynard@proton.me>
2025-12-14 01:43:15 -0600

    update about

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -282,5 +282,5 @@ export const MY_PROJECTS: Project[] = [
 export const ABOUT = {
   title: "Hi, I'm Jacob",
   description:
-    "I'm passionate about building awesome applications that bring value to people. I enjoy learning all I can as a student and software developer, and love exploring new things and applying that knowledge. I'm currently pursuing a Masters in Artificial Intelligence at Saint Louis University. My goal is to pursue a career as a software developer and apply my skills in JavaScript and C# to build powerful and performant applications.",
+    "I'm passionate about building applications that bring value to people. I enjoy learning all I can as a student and software developer, and love exploring new things and applying that knowledge. I'm currently pursuing a Masters in Artificial Intelligence at Saint Louis University. My goal is to pursue a career as a software developer and work with awesome peoeple to continue learning and building cool stuff.",
 };
